2928ecea49db86b0b203884307784e18d235b96f
GhosTHaise <ghostrex2@gmail.com>
2026-04-12 19:12:04 +0300

    feat : load font

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -3,15 +3,17 @@ import { styled } from 'nativewind';
 import { Text } from "react-native";
 import { SafeAreaView as RNSafeAreaView } from 'react-native-safe-area-context';
 
-
 export default function App() {
   const SafeAreaView = styled(RNSafeAreaView);
   return (
     <SafeAreaView className="flex-1 bg-background p-5">
-      <Text className="text-xl font-bold text-blue-500">
-        Welcome to Nativewind!
+      <Text className="text-7xl font-sans-extrabold">
+        Home
       </Text>
-      <Link href="/onboarding" className="mt-4 rounded bg-primary text-white p-4">
+      <Text className="text-7xl font-bold">
+        Home
+      </Text>
+      <Link href="/onboarding" className="mt-4 rounded bg-primary text-white p-4 font-sans-bold">
         Go to onboarding
       </Link>
     </SafeAreaView>
