cf06e7e9d81dc2e53c9c27538bf39dd7a4412fff
GhosTHaise <ghostrex2@gmail.com>
2026-04-15 20:33:21 +0300

    feat : balance section

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,6 +1,8 @@
-import { HOME_USER } from '@/constants/data';
+import { HOME_BALANCE, HOME_USER } from '@/constants/data';
 import { icons } from '@/constants/icons';
 import images from '@/constants/images';
+import { formatCurrency } from '@/lib/utils';
+import dayjs from 'dayjs';
 import { styled } from 'nativewind';
 import { Image, Text, View } from "react-native";
 import { SafeAreaView as RNSafeAreaView } from 'react-native-safe-area-context';
@@ -15,7 +17,17 @@ export default function App() {
           <Text className="home-user-name">{HOME_USER.name}</Text>
         </View>
 
-        <Image source={icons.add} className="home-add-logo" />
+        <Image source={icons.add} className="home-add-icon" />
+      </View>
+
+      <View className="home-balance-card">
+        <Text className="home-balance-label">Balance</Text>
+        <View className="home-balance-row">
+          <Text className="home-balance-amount">
+            {formatCurrency(HOME_BALANCE.amount)}
+          </Text>
+          <Text className="home-balance-date">{dayjs(HOME_BALANCE.nextRenewalDate).format('MM/YY')}</Text>
+        </View>
       </View>
     </SafeAreaView>
   );
