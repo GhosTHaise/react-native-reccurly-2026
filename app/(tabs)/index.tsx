43d43650f492e711cae0f92edbe0565b84a19340
GhosTHaise <ghostrex2@gmail.com>
2026-04-18 20:41:45 +0300

    feat : add container classnam,e

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -65,6 +65,7 @@ export default function App() {
         ItemSeparatorComponent={() => <View className="h-4" />}
         showsVerticalScrollIndicator={false}
         ListEmptyComponent={<Text className="home-empty-state">No subscriptions yet.</Text>}
+        contentContainerClassName='pb-30'
       />
     </SafeAreaView>
   );
